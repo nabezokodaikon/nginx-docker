9dc889fdbd15b1582d0b710c070ffac6314e54f4
main <main@example.com>
2014-11-09 15:58:07 +0900

    ポート番号を変更。

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ fi
 
 docker stop nginx
 docker rm nginx
-docker run --name nginx --link gitbucket:gitbucket -v ${PWD}/log:/var/log/nginx -d -p 80:80 -p 8080:8080 -t nabezokodaikon/ubuntu:nginx
+docker run --name nginx --link gitbucket:gitbucket -v ${PWD}/log:/var/log/nginx -d -p 50080:80 -p 58080:8080 -t nabezokodaikon/ubuntu:nginx
